cbf50a38c676a9669278407161198c323b66eb30
francesco-source <66483915+francesco-source@users.noreply.github.com>
2020-10-24 18:32:07 +0200

    Create ResonanceType.cpp

---
+++ ResonanceType.cpp
@@ -0,0 +1,16 @@
+#include "ResonanceType.hpp"
+
+
+
+double ResonanceType::getWidth() const {
+	return fWidth;
+}
+
+
+void ResonanceType::print() const {
+	PartycleType::print();
+	std::cout << "| Resonance width:" << fWidth << "\n";
+}
+
+ResonanceType::ResonanceType(const char* name) : fWidth{ 0 }, PartycleType(0, 0, name){};
+ResonanceType::ResonanceType(double width, int coulomb, double mass,const char* name) : fWidth{ width }, PartycleType(coulomb, mass, name){};
