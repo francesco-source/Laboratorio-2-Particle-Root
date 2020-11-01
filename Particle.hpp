d5adb45e5564497007058055a5418a3e78a80807
francesco-source <66483915+francesco-source@users.noreply.github.com>
2020-11-01 23:35:05 +0100

    Update Particle.hpp

--- Particle.hpp
+++ Particle.hpp
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef PARTICLE_HPP
+#define PARTICLE_HPP
 
 #include"ResonanceType.hpp"
 
@@ -13,27 +14,29 @@
 	public:
 		Particle(const char* name, double Px, double Py, double Pz);
 		Particle();
-		 int getMax_Num_ParticleType() const;
-		int get_Num_ParticleType() const ;
-		int get_index() const;
-		auto getCharge() const;
-		double getMass() const;
-		auto getName() const;
-		auto get_p_x() const;
-		auto get_p_y() const ;
-		auto get_p_z() const;
-		auto getWidth() const;
+		 int GetMax_Num_ParticleType() const;
+		int Get_Num_ParticleType() const ;
+		int Get_index() const;
+		int GetCharge() const;
+		double GetMass() const;
+		const char* GetName() const;
+		double Get_p_x() const;
+		double Get_p_y() const ;
+		double Get_p_z() const;
+		double GetWidth() const;
 		static void AddParticleType(const char*,int , double,double=0);
 		void setter(int i);
 		void setter(const char*);
-		void set_p(int ,int ,int);
+		void SetP(double,double,double);
 		static void print_array();
 		void print_particle() const;
-		double Energy_of_Particle() const;
+		double GetEnergy() const;
 		double Mass_invariant(Particle& p)const;
 		double momentum_module() const;
+		int Decay2body(Particle &dau1,Particle &dau2) const;
 	private:
 		int static FindParticle(const char* name);
+		void Boost(double bx, double by, double bz);
 	    static const int fMaxNumParticleType=10;
 		static int fNParticleType;
 		static PartycleType* fParticleType[fMaxNumParticleType];
@@ -41,4 +44,7 @@
 		double fPx=0;
 		double fPy=0;
 		double fPz=0;
+		
 	};
+
+#endif
