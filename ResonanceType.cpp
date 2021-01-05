02e5e3a99c375bfd92f517a66ffa1b3ca2b827c5
francesco-source <66483915+francesco-source@users.noreply.github.com>
2021-01-05 19:21:49 +0100

    Update ResonanceType.cpp

--- ResonanceType.cpp
+++ ResonanceType.cpp
@@ -1,16 +1,17 @@
 #include "ResonanceType.hpp"
 
-
-
-double ResonanceType::getWidth() const {
-	return fWidth;
-}
-
+double ResonanceType::GetWidth() const 
+{ return fWidth; }
 
 void ResonanceType::print() const {
-	PartycleType::print();
-	std::cout << "| Resonance width:" << fWidth << "\n";
+  PartycleType::print();
+  std::cout << "| Resonance width:" 
+	  << fWidth << "\n";
 }
 
-ResonanceType::ResonanceType(const char* name) : fWidth{ 0 }, PartycleType(0, 0, name){};
-ResonanceType::ResonanceType(double width, int coulomb, double mass,const char* name) : fWidth{ width }, PartycleType(coulomb, mass, name){};
+ResonanceType::ResonanceType(const char *name)
+    : fWidth{0}, PartycleType(0, 0, name){};
+ResonanceType::ResonanceType(double width, int coulomb,
+			     double mass,
+                             const char *name)
+    : fWidth{width}, PartycleType(coulomb, mass, name){};
