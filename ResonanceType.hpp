9bce1d835047c296406d2b02d72b0f582c107a47
francesco-source <66483915+francesco-source@users.noreply.github.com>
2021-01-05 19:26:07 +0100

    Update ResonanceType.hpp

--- ResonanceType.hpp
+++ ResonanceType.hpp
@@ -1,19 +1,15 @@
-#pragma once
-#include"Partycletype.hpp"
+#ifndef RESONANCETYPE_HPP
+#define RESONANCETYPE_HPP
+
+#include "ParticleType.hpp"
 
 class ResonanceType : public PartycleType {
-   public:
-	   ResonanceType(double width,int charge, double mass,const char* name);
-	   ResonanceType(const char* name);
-	   double getWidth() const override;
-	   void print() const override;
-
-
-
-
+public:
+  ResonanceType(double width, int charge, double mass, const char *name);
+  ResonanceType(const char *name);
+  double GetWidth() const override;
+  void print() const override;
 
 private:
-	const double fWidth=0;
-
-
+  const double fWidth = 0;
 };
